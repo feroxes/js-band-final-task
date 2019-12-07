e63d259a5da516423aef368cef701e20711a525c
Yaroslav Harmash <feroxes@gmail.com>
2019-12-07 16:40:43 +0200

    CFT-003: + created header component

---
+++ src/components/ui/BasketIcon/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+function BasketIcon({ countOfProducts }) {
+  return (
+    <div className="position-relative">
+      <img
+        style={{ width: '50px' }}
+        src="https://img.icons8.com/dotty/80/000000/shopping-basket.png"
+        alt="Basket"
+        className="mx-2"
+      />
+      <div
+        className="d-flex justify-content-center align-items-center position-absolute bg-success rounded-circle "
+        style={{ width: '18px', height: '18px', fontSize: '12px', top: 0, right: 0 }}
+      >
+        {countOfProducts}
+      </div>
+    </div>
+  );
+}
+
+BasketIcon.propTypes = {
+  countOfProducts: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+  countOfProducts: state.basket.countOfProducts,
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(BasketIcon);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import basket from './basket';
 
 export default combineReducers({
   user,
+  basket,
 });
