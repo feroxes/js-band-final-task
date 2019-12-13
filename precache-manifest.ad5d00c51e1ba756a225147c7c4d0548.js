5828b4a227deb748719668356600c8095ab0531c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-13 12:38:54 +0000

    Deploy feroxes/js-band-final-task to github.com/feroxes/js-band-final-task.git:gh-pages

---
+++ precache-manifest.ad5d00c51e1ba756a225147c7c4d0548.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65f33b4bcbe1009f59f1138bd4a8d1c",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "4c02d34e62048075812d",
+    "url": "/js-band-final-task/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "f2fada49f9ca9cdaf525",
+    "url": "/js-band-final-task/static/css/main.7c396fe8.chunk.css"
+  },
+  {
+    "revision": "4c02d34e62048075812d",
+    "url": "/js-band-final-task/static/js/2.ea5e208f.chunk.js"
+  },
+  {
+    "revision": "f2fada49f9ca9cdaf525",
+    "url": "/js-band-final-task/static/js/main.e5f7b83c.chunk.js"
+  },
+  {
+    "revision": "83440d9fdeca7baaff28",
+    "url": "/js-band-final-task/static/js/runtime-main.e195148a.js"
+  },
+  {
+    "revision": "f746c224f95dd153deb328f2013d7ab4",
+    "url": "/js-band-final-task/static/media/bg2.f746c224.jpg"
+  },
+  {
+    "revision": "f42d312bb5021075c0307133dc64f9f6",
+    "url": "/js-band-final-task/static/media/notFound.f42d312b.jpg"
+  },
+  {
+    "revision": "65ebb302e2bd7306a2fdc51eec5ddaf0",
+    "url": "/js-band-final-task/static/media/spinner.65ebb302.gif"
+  }
+]);
